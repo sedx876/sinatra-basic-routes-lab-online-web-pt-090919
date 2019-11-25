dc107e25c3faece5ce8436c97dc0ea8e86f0d4bf
Sharon Watkins <swatkins876@gmail.com>
2019-11-25 17:06:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Sharon"
+  end
+
+  get '/hometown' do
+    "My hometown is Syracuse"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Glass House"
+  end
 end
